7c89c4710445bb7603558e4e691b6709ffe31180
jmshim95 <jmshim95@gmail.com>
2022-05-27 11:27:58 +0900

    설정 및 구본구조

---
+++ src/main/java/com/book/config/RootConfig.java
@@ -0,0 +1,50 @@
+package com.book.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = ("com.book.*"))
+@MapperScan(basePackages = ("com.book.mapper"))
+@EnableScheduling
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
+public class RootConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("scott");
+		hikariConfig.setPassword("tiger"); 
+		
+		return new HikariDataSource(hikariConfig);
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		sqlSessionFactory.setTypeAliasesPackage("com.book.vo");
+		return (SqlSessionFactory)sqlSessionFactory.getObject();
+	}
+
+	@Bean
+	public DataSourceTransactionManager txManager(){
+		return new DataSourceTransactionManager(dataSource());
+	}
+}
